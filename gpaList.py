c5ac862e4cf9c352df783c158c63fec62071644b
The0Cracklinz <77657337+The0Cracklinz@users.noreply.github.com>
2023-09-03 13:43:10 -0400

    Add files via upload

---
+++ gpaList.py
@@ -0,0 +1,14 @@
+#David Ocaranza
+#gpaList
+#Enter student details and decides list made
+
+lastName=input("what is students's last name? ")
+if (lastName=="ZZZ"):
+    exit(0)
+firstName=input("what is students's first name? ")
+GPA=input("Enter your gpa ")
+GPA=float(GPA)
+if (GPA>=3.5):
+    print("Student has made the Dean's List ")
+if (GPA>=3.5):
+    print("Studnet has made the Honor Roll ")
